b17e094b5bfb8f76a98415f6a30030c6fc40ba2b
hemantsha0808-byte <hemant.sha.08082002@gmail.com>
2026-09-23 05:07:48 +0000

    Delete 2.MgperdlToMmolperL.py

--- 2.MgperdlToMmolperL.py
+++
@@ -1,7 +0,0 @@
-x = float(input("Enter concentration in mg/dl: "))
-
-molar_mass = float(input("Enter molar mass of molecule: "))
-
-y = x / molar_mass * 10
-
-print("Concentration in mmol/L is: ", y)
